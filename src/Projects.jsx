3f316ba0c80f231cd2a446640c716f165f1f6f43
August <august.enhager@gmail.com>
2021-09-29 14:48:50 +0200

    adds menu items and tabs

---
+++ src/Projects.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Container } from "semantic-ui-react"
+
+const Projects = () => {
+  return (
+    <Container>
+      <h1 id ="projects-header">My Projects</h1>
+      <p>
+        Ipsum dolor dolorem.
+      </p>
+      
+    </Container>
+  )
+}
+
+export default Projects
